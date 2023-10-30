77d6d3901a06e8046a01d95f0e282da16b949d43
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-10-30 15:50:54 +0530

    Added controller and route for upload image to cloudinary

--- controllers/fileUpload_controller.js
+++ controllers/fileUpload_controller.js
@@ -1,5 +1,5 @@
 const File = require('../models/File');
-
+const cloudinary = require('cloudinary').v2
 //localfileupload  --> handler function
 exports.localFileUpload = async(req, res) => {
     try {
@@ -23,4 +23,61 @@ exports.localFileUpload = async(req, res) => {
         console.log(error);
     }
 
+}
+
+function isFileTypeSupported(type, supportedTypes){
+    return supportedTypes.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder){
+    const options = {folder}
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+//image upload handler
+exports.imageUpload = async(req, res) => {
+    try {
+        // fetch data from request
+        const{name, tags, email} = req.body;
+        //console.log(name, tags, email);
+
+        const file = req.files.imageFile;
+        //console.log("file", file);
+
+        //validation
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        // console.log("fileType",fileType);
+
+        // file format not supported
+        if(!isFileTypeSupported(fileType, supportedTypes)){
+            return res.status(400).json({
+                success:false,
+                message:'File formate is not supported'
+            })
+        }
+
+        // if file format supported
+        const response = await uploadFileToCloudinary(file, "content");
+        console.log(response);
+        // save entry into db
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl: response.secure_url
+        });
+        res.json({
+            success: true,
+            imageUrl: response.secure_url,
+            message: "Image Successfully Uploaded"
+        });
+
+
+    } catch (error) {
+       console.log(error);
+       res.status(400).json({
+        success:false,
+        message:"Something went wrong"
+       });
+    }
 }
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // taking all handeler from the controller
-const {localFileUpload} = require('../controllers/fileUpload_controller');
+const {localFileUpload, imageUpload} = require('../controllers/fileUpload_controller');
 
 // define api rout
 router.post("/localFileUpload", localFileUpload);
+router.post("/imageUpload", imageUpload);
 
 module.exports = router; 
